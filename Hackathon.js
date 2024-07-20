0e8795eab9bfcae7cd455fb835eaca7780946a0d
MichaelFine2022 <114603775+MichaelFine2022@users.noreply.github.com>
2024-07-20 13:15:18 -0700

    Create Hackathon.js

---
+++ Hackathon.js
@@ -0,0 +1,5 @@
+function addressForm() {
+            var address1 = document.getElementById("address1").value;
+            var address2 = document.getElementById("address2").value;
+            return false;
+}
